de73c6ca1d2c2ace5238b5a5442a9091703a2e56
cristsr <styven21121@gmail.com>
2022-02-19 17:15:13 -0500

    chore: automapper dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,18 +4,22 @@ import { DatabaseModule } from 'database/database.module';
 import { validate } from 'config/validator';
 import { AppController } from './app.controller';
 import { CategoryModule } from 'app/category/category.module';
-import { SharedModule } from 'app/shared/shared.module';
 import { MovementModule } from 'app/movement/movement.module';
+import { AutomapperModule } from '@automapper/nestjs';
+import { classes } from '@automapper/classes';
 
 @Module({
   imports: [
+    AutomapperModule.forRoot({
+      options: [{ name: 'mapper', pluginInitializer: classes }],
+      singular: true,
+    }),
     ConfigModule.forRoot({
       isGlobal: true,
       validate: validate,
     }),
     DatabaseModule,
     CategoryModule,
-    SharedModule,
     MovementModule,
   ],
   controllers: [AppController],
