626ae601af37f9dc72a0a3c4196215956f1c5d89
cristsr <styven21121@gmail.com>
2022-04-19 16:26:38 -0500

    feat: summary module

--- src/app/summary/entities/summary.entity.ts
+++ src/app/summary/entities/summary.entity.ts
@@ -3,15 +3,14 @@ import { ViewColumn, ViewEntity } from 'typeorm';
 @ViewEntity({
   name: 'summary',
   expression: `
-      select m.id,
-             m.amount,
-             c.name,
-             c.color,
-             m.date
-      from movements m
-               left join categories c on m.category_id = c.id
-      where m.type = 'expense'
-        and to_char(m.date, 'YYYY-MM') = to_char(current_date, 'YYYY-MM')
+    select 
+      m.id,
+      m.amount,
+      c.name,
+      c.color,
+      m.date
+    from movements m left join categories c on m.category_id = c.id
+    where m.type = 'expense' and to_char(m.date, 'YYYY-MM') = to_char(current_date, 'YYYY-MM')
   `,
 })
 export class SummaryEntity {
--- src/app/summary/services/summary.service.ts
+++ src/app/summary/services/summary.service.ts
@@ -28,63 +28,77 @@ export class SummaryService {
 
   async summary(): Promise<Record<string, any>> {
     const balance = await this.balanceRepository.findOne();
-    const pieStats = await this.generatePieStats();
-    const barStats = await this.generateBarStats();
+    const pie = await this.generateExpensesByPeriod();
+    const bar = await this.generateExpensesByWeek();
+    const latestMovements = await this.getLatestMovements();
 
-    return { balance, pieStats, barStats };
+    return {
+      balance,
+      pie,
+      bar,
+      latestMovements,
+    };
   }
 
-  private async generatePieStats(): Promise<any> {
+  private async generateExpensesByPeriod(): Promise<any> {
     try {
       const daily = await this.summaryRepository
         .createQueryBuilder()
-        .select('SUM(amount) as amount, name, color')
+        .select('cast(SUM(amount) as real) as amount, name, color')
         .where('date = current_date')
         .groupBy('amount, name, color')
+        .orderBy('amount', 'DESC')
+        .take(5)
         .getRawMany();
 
       const weekly = await this.summaryRepository
         .createQueryBuilder()
-        .select('SUM(amount) as amount, name, color')
+        .select('cast(SUM(amount) as real) as amount, name, color')
         .where(
           `to_char(date, 'YYYY-MM-W') = to_char(current_date, 'YYYY-MM-W')`,
         )
         .groupBy('amount, name, color')
+        .orderBy('amount', 'DESC')
+        .take(5)
         .getRawMany();
 
       const monthly = await this.summaryRepository
         .createQueryBuilder()
-        .select('SUM(amount) as amount, name, color')
+        .select('cast(SUM(amount) as real) as amount, name, color')
         .groupBy('amount, name, color')
+        .orderBy('amount', 'DESC')
+        .take(5)
         .getRawMany();
 
-      return { daily, weekly, monthly };
+      return {
+        daily,
+        weekly,
+        monthly,
+      };
     } catch (e) {
       this.logger.error(`Error generating pie stats: ${e.message}`);
       throw new UnprocessableEntityException(e.message);
     }
   }
 
-  private async generateBarStats(): Promise<any> {
+  private async generateExpensesByWeek(): Promise<any> {
     const today = DateTime.local();
 
-    const daysAgo = new Array(7).fill(0).map((_, i: number) => ({
+    const days = new Array(7).fill(0).map((_, i: number) => ({
       locale: today.minus({ days: i }).toLocaleString({ weekday: 'short' }),
       format: today.minus({ days: i }).toFormat('yyyy-MM-dd'),
     }));
 
     const result = [];
 
-    for (const day of daysAgo) {
+    for (const day of days) {
       const query = this.summaryRepository
         .createQueryBuilder()
         .select('cast(SUM(amount) as real)', 'amount')
-        .where(`date = :date`, { date: day.format })
-        .groupBy('amount, name, color')
-        .getRawOne();
+        .where(`date = :date`, { date: day.format });
 
       try {
-        const record = await query;
+        const record = await query.getRawOne();
 
         result.push({
           day: day.locale,
@@ -92,6 +106,7 @@ export class SummaryService {
         });
       } catch (e) {
         this.logger.error(`Error generating bar stats: ${e.message}`);
+
         result.push({
           day: day.locale,
           amount: 0,
@@ -100,5 +115,15 @@ export class SummaryService {
     }
 
     return result;
+  }
+
+  getLatestMovements(): Promise<MovementEntity[]> {
+    return this.movementRepository.find({
+      relations: ['category', 'subcategory'],
+      order: {
+        date: 'DESC',
+      },
+      take: 5,
+    });
   }
 }
